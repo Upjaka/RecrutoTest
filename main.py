bdd25bea21d690639dc22a11ebdcbe19fe49a043
Alexandr Ludov <alesant999@gmail.com>
2025-02-26 16:37:31 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello(name: str = "Recruto", message: str = "Давай дружить!"):
+    return {f"Hello {name}! {message}"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
